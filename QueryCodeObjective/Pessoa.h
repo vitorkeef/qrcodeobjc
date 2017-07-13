7cb289d977f5f3e010cb28054aa8a090affd997a
ImacTeamMobile2 <imacteammobile2@admins-iMac.local>
2017-07-13 14:50:44 -0300

    error

---
+++ QueryCodeObjective/Pessoa.h
@@ -0,0 +1,13 @@
+//
+//  Pessoa.h
+//  QueryCodeObjective
+//
+//  Created by ImacTeamMobile2 on 12/07/17.
+//  Copyright © 2017 Humberto Puccinelli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pessoa : NSObject
+
+@end
---
+++ QueryCodeObjective/Pessoa.m
@@ -0,0 +1,13 @@
+//
+//  Pessoa.m
+//  QueryCodeObjective
+//
+//  Created by ImacTeamMobile2 on 12/07/17.
+//  Copyright © 2017 Humberto Puccinelli. All rights reserved.
+//
+
+#import "Pessoa.h"
+
+@implementation Pessoa
+
+@end
